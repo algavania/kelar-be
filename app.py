c9591f26fa253cdba0a309e40184841c27ec8457
Alga <vaniaalga@gmail.com>
2024-07-23 18:25:52 +0700

    fix: change port

--- app.py
+++ app.py
@@ -90,5 +90,5 @@ def sendResponse(status, data):
         "data": data
     })
     
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=4000)
