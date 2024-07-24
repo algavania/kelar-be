f895d1868965e38e10d0d1c8a6385e917dea3e9f
Alga <vaniaalga@gmail.com>
2024-07-24 21:14:48 +0700

    fix: update port

--- app.py
+++ app.py
@@ -75,5 +75,6 @@ def send_response(status, data):
     })
     
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.getenv("PORT")
+    port = int(os.getenv("PORT", 4000))
+    app.run(host='0.0.0.0', port=port)
 )
